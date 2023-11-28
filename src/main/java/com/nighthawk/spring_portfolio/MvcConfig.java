ee92a3ff2f57044ef1015bbbf6c63593b624abeb
rohinsood <rs.rohinsood@gmail.com>
2023-11-28 11:55:23 -0800

    update security config

--- src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
@@ -6,12 +6,6 @@ import org.springframework.web.servlet.config.annotation.*;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-    // set up your own index
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
-    }
-
     /* map path and location for "uploads" outside of application resources
        ... creates a directory outside "static" folder, "file:volumes/uploads"
        ... CRITICAL, without this uploaded file will not be loaded/displayed by frontend
--- src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
@@ -36,9 +36,6 @@ public class SecurityConfig {
 				)
 				// list the requests/endpoints need to be authenticated
 				.authorizeHttpRequests(auth -> auth
-					.requestMatchers("/authenticate").permitAll()
-					.requestMatchers("/mvc/person/update/**", "/mvc/person/delete/**").authenticated()
-					.requestMatchers("/api/person/**").authenticated()
 					.requestMatchers("/**").permitAll()
 				)
 				// support cors
