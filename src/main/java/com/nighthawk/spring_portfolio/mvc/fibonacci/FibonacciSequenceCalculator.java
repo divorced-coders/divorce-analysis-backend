c374aec29e6a40d074be86c0624c8b0d07f5d552
paravsalaniwal <paravsalaniwal@gmail.com>
2023-12-01 12:11:11 -0800

    commit for the fib seq

--- src/main/java/com/nighthawk/spring_portfolio/mvc/fibonacci/FibonacciSequenceCalculator.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/fibonacci/FibonacciSequenceCalculator.java
@@ -10,19 +10,27 @@ public class FibonacciSequenceCalculator {
 
     // Ideally O(n) Time & O(n) Space
     public static int fibonacci(int n) {
+        // Base case: if n is 0 or 1, return n
         if (n <= 1) {
             return n;
         }
-
+    
+        // Check if the result for the current value of n is already memoized
         if (memo.containsKey(n)) {
+            // Return the memoized result to avoid redundant calculations
             return memo.get(n);
         }
-
+    
+        // Recursive calculation for Fibonacci sequence
         int result = fibonacci(n - 1) + fibonacci(n - 2);
+    
+        // Memoize the result for the current value of n
         memo.put(n, result);
-
+    
+        // Return the calculated result
         return result;
     }
+    
 
     // O(2^n) Time & O(n) Space
     public static int fibonacci(Integer n) {
